9199900cbb4c4cf2545b8a83ae421523c3642433
Tristan Daniels <tdaniels@sessionm.com>
2018-05-11 17:08:32 -0400

    Updated login screen.

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     private let testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOiIyMDE3LTA5LTI3IDE1OjMwOjU1ICswMDAwIiwiZXhwIjoiMjAxNy0xMC0xMSAxNTozMDo1NSArMDAwMCIsImRhdGEiOnsiaWQiOiJkYTYxZGNkYS1hMzk4LTExZTctODcxZi05ZjZkNTQzYmUwNDAifX0.iBrHv9-INszE-SSL9rsuNnLDv7DBBaIUuqM6XDUvecxzap2CuoN4v3juXPvw-dZWuzbiHY2H3TPJJlRcI5_fZPxH2FjDqGA1S5nwEwEYVn9D1oMvnXUB6jLIq3ev4omE7ZUj5zVytsn_rKdryllfHro_8g5TneiOUoFBa_1N_RcC9AK_8640xbYPtZaNWhxsJiCwTsKWaLSYQ6RQv_xo1M4reL56dbjJ16Y-50HUy6Pxax6biKVvpjNRDizrkY0bka07lHMLAHMZD5-D3OYnxpxyg9aVX2kJd36iZuwsKaXVMtrCzwmzzGuhQD1PUUhC43wkNUbYw9z2d94v0FDxvQ"
-    private let testEmail = "test@sessionm.com"
-    private let testPassword = "aaaaaaaa1"
+    private let testEmail = "sampleuser@sessionm.com"
+    private let testPassword = "sessionm1"
 
     private var emailRegex: NSRegularExpression!
 
@@ -73,25 +73,46 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 //    }
 
     @IBAction private func oauthLoginUser(_ sender: UIButton) {
-        if let email = email.text, let password = password.text {
-            if let provider = SessionM.authenticationProvider() as? SessionMOAuthProvider {
-                provider.authenticateUser(withEmail: email, password: password) { (state: SMAuthState, error: SMError?) in
-                    if error == nil {
-                        self.dismiss(animated: true)
-                    }
-                    print("Authenticated: \(email)");
+        if let email = email.text, let password = password.text, let provider = SessionM.authenticationProvider() as? SessionMOAuthProvider {
+            provider.authenticateUser(withEmail: email, password: password) { (state: SMAuthState, error: SMError?) in
+                if error == nil {
+                    self.dismiss(animated: true)
+                }
+                print("Authenticated: \(email)");
+            }
+        }
+    }
+
+    @IBAction private func loginSampleUser(_ sender: UIButton) {
+        if let provider = SessionM.authenticationProvider() as? SessionMOAuthProvider {
+            provider.authenticateUser(withEmail: testEmail, password: testPassword) { (state: SMAuthState, error: SMError?) in
+                if error == nil {
+                    self.dismiss(animated: true)
+                }
+                print("Authenticated: \(self.testEmail)");
+            }
+        }
+    }
+
+    @IBAction private func authenticate(_ sender: UIButton) {
+        if let token = token.text, let provider = SessionM.authenticationProvider() as? SessionMOAuthProvider {
+            provider.authenticateUser(withToken: token, provider: "api_auth_token") { (state: SMAuthState, error: SMError?) in
+                if error == nil {
+                    self.dismiss(animated: true)
                 }
             }
         }
     }
 
-//    @IBAction private func loginSampleUser(_ sender: UIButton) {
-//        sessionM.logInUser(withEmail: testEmail, password: testPassword)
-//    }
-//
-//    @IBAction private func authenticateSampleUser(_ sender: UIButton) {
-//        sessionM.authenticate(withToken: testToken, provider: "api_auth_token")
-//    }
+    @IBAction private func authenticateSampleUser(_ sender: UIButton) {
+        if let provider = SessionM.authenticationProvider() as? SessionMOAuthProvider {
+            provider.authenticateUser(withToken: testToken, provider: "api_auth_token") { (state: SMAuthState, error: SMError?) in
+                if error == nil {
+                    self.dismiss(animated: true)
+                }
+            }
+        }
+    }
 
 //    func sessionM(_ sessionM: SessionM, didTransitionToState state: SessionMState) {
 //        if let label = sessionState {
